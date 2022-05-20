16a95a2a5782dc3cdcfba67d6cb4ad060daa9e2d
Jose Alexander Vindel Garduno <60977557+j-alex-vindel@users.noreply.github.com>
2022-05-20 17:32:37 +0100

    Update Bafa_distances.py

--- Bafa_distances.py
+++ Bafa_distances.py
@@ -1,6 +1,7 @@
 import multiprocessing
 import os
 from bafa_scrapper import distance_crawler,division_distance
+import pandas as pd
 
 divisions = ['NFC 1 North','NFC 1 South','NFC 2 North','NFC 2 South','NFC 2 West','Premiership North',
 'Premiership South','SFC 1 East','SFC 1 West','SFC 2 North','SFC 2 South','SFC 2 West']
@@ -14,4 +15,11 @@ if __name__ == '__main__':
 
     for process in processes:
         process.join()
-
+        
+    master_dist = []
+    for division in divisions:
+        dispdf = pd.read_csv('BAFA_2019_D_%s.csv'%division,index_col=False)
+        master_dist.append(dispdf)
+    master_distance_df = pd.concat(master_dist,ignore_index=True)
+    
+    master_distance_df.to_csv('BAFA_D_2019_All.csv',header=True,index=False)
